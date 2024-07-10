36be87712bedffbc6399b73011c8bd92e5eeae76
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2024-07-10 15:04:51 +0900

    SEO

--- app.vue
+++ app.vue
@@ -3,3 +3,30 @@
     <NuxtPage></NuxtPage>
   </div>
 </template>
+
+
+<script setup lang="ts">
+interface MetaObject {
+  title?: string;
+  meta?: {
+    hid: string; 
+    name: string; 
+    content: string;
+  } 
+}
+
+useHead<MetaObject>({
+  title: 'TodoList Project 입니다.',
+  meta: [
+    { hid: 'description', name: 'description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'og:title', property: 'og:title', content: 'TodoList-nuxt' },
+    { hid: 'og:description', property: 'og:description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'og:image', property: 'og:image', content: '/assets/images/mainpage.png' },
+    { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+    { hid: 'twitter:title', name: 'twitter:title', content: 'TodoList-nuxt' },
+    { hid: 'twitter:description', name: 'twitter:description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'twitter:image', name: 'twitter:image', content: '/assets/images/mainpage.png' },
+    { name: 'keywords', content: 'TodoList, 할 일 관리, 일정, 일정 관리, Nuxt.js'}
+  ],
+})
+</script>
--- pages/TodoMain/index.vue
+++ pages/TodoMain/index.vue
@@ -16,6 +16,21 @@
 </template>
 
 <script setup lang="ts">
+definePageMeta({
+  title: 'TodoList Project 입니다.',
+  meta: [
+    { hid: 'description', name: 'description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'og:title', property: 'og:title', content: 'TodoList-nuxt' },
+    { hid: 'og:description', property: 'og:description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'og:image', property: 'og:image', content: '/assets/images/main.png' },
+    { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+    { hid: 'twitter:title', name: 'twitter:title', content: 'TodoList-nuxt' },
+    { hid: 'twitter:description', name: 'twitter:description', content: '사용자의 할 일을 관리할 수 있는 프로젝트입니다.' },
+    { hid: 'twitter:image', name: 'twitter:image', content: '/assets/images/main.png' },
+    { name: 'keywords', content: 'TodoList, 할 일 관리, 일정, 일정 관리, Nuxt.js'}
+  ],
+})
+
 import tap from './components/tap.vue';
 import HeaderSection from './components/HeaderSection.vue';
 import TodoContents from './components/TodoContents.vue';
